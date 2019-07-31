b36cd2cc8ba851860d3108b059595031b238c070
Md. Emruz Hossain <emruz@appscode.com>
2019-07-31 14:28:05 +0600

    Don't set annotation to AppBinding (#159)
    Why?
    - Annotation is used for auto backup in Stash. If we set annotation in AppBinding from KubeDB then the auto backup annotation will get lost if any update happen to database crd.

--- pkg/controller/appbinding.go
+++ pkg/controller/appbinding.go
@@ -37,7 +37,6 @@ func (c *Controller) ensureAppBinding(db *api.MySQL) (kutil.VerbType, error) {
 	_, vt, err := appcat_util.CreateOrPatchAppBinding(c.AppCatalogClient, meta, func(in *appcat.AppBinding) *appcat.AppBinding {
 		core_util.EnsureOwnerReference(&in.ObjectMeta, ref)
 		in.Labels = db.OffshootLabels()
-		in.Annotations = db.Spec.ServiceTemplate.Annotations
 
 		in.Spec.Type = appmeta.Type()
 		in.Spec.Version = mysqlVersion.Spec.Version
